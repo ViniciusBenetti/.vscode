bad4683f52766efe8ccd1cb3fb59923acd06c0fc
ViniciusBenetti <makako1289@gmail.com>
2023-08-13 17:29:39 -0300

    c/c++

---
+++ projeto1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+float area(float largura, float comprimento){ 
+
+    return largura * comprimento;
+    }
+
+int main(){
+    float a,b;
+
+
+
+    std::cout << "digite a largura:\n";
+    std::cin >> a;
+    std::cout << "digite o comprimento:\n";
+    std::cin >> b;
+    std::cout << "area: "<< area(a,b) << std::endl;
+
+}
+
